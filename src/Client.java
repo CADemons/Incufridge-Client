ea0aa1c1ba8012401ed222c36953f0480a66c719
Linc <lincb123@gmail.com>
2013-09-22 12:07:46 -0400

    Added basic communication
    Also moved libs to bin

---
+++ src/Client.java
@@ -0,0 +1,17 @@
+
+public class Client {
+	
+	public static void main(String[] args){
+		SerialComm main = new SerialComm();
+		main.initialize();
+		Thread t=new Thread() {
+			public void run() {
+				//the following line will keep this app alive for 1000 seconds,
+				//waiting for events to occur and responding to them (printing incoming messages to console).
+				try {Thread.sleep(1000000);} catch (InterruptedException ie) {}
+			}
+		};
+		t.start();
+		System.out.println("Started");
+	}
+}
