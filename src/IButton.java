4ece825df028576bdb1bfd7cc9ac07d1ebb8d395
zyedidia <zachary@Zacharys-MacBook-Air.local>
2014-01-13 11:08:36 -0500

    Added check to see if entered number is over the byte range (128) when uploading

--- src/IButton.java
+++ src/IButton.java
@@ -48,8 +48,9 @@ public class IButton {
 
 	public void onClick() {		
 		for(int c = 0; c < app.inputs.length; c++){
-			if(app.inputs[c].intext != ""){
+			if(app.inputs[c].intext != "" && Integer.parseInt(app.inputs[c].intext) < 128) {
 				SerialComm.temps[c] = Byte.parseByte(app.inputs[c].intext);
+				System.out.println("Uploaded: " + app.inputs[c].intext);
 			} else {
 				SerialComm.temps[c]=0;
 			}
