06d614b3e13c0aa65ee6d537c927bd2ed363cd8f
Zach <pi@raspberrypi.(none)>
2015-02-02 11:47:25 -0500

    Fixed libraries and retry connection prompt

--- src/swing/ConnectionPanel.java
+++ src/swing/ConnectionPanel.java
@@ -13,53 +13,56 @@ import common.SerialConnector;
  * Incu-Fridge */
 @SuppressWarnings("serial")
 public class ConnectionPanel extends JPanel {
-	
+
 	// Used to perform serial operations
 	private SerialConnector serial;
 	// Textarea used to give feedback to the user about the connection
 	private JTextArea connectionStatus;
 	// Allows the user to try to reconnect if the connection failed
 	private JButton retryButton;
-	
+
 	// The main SerialConnector object is passed in the constructor by the GUI
 	public ConnectionPanel(SerialConnector serial) {
 		this.serial = serial;
-		
+
 		connectionStatus = new JTextArea(5, 30);
 		connectionStatus.setEditable(false);
 		connectionStatus.setLineWrap(true);
 		retryButton = new JButton("Retry Connection");
-		
+
 		// Attempt to connect
-		connect();
-		
+		connect(false);
+
 		AL AL = new AL();
-		
+
 		retryButton.addActionListener(AL);
-		
+
 		// Add the components to the panel
 		this.add(connectionStatus);
 		this.add(retryButton);
 	}
-	
+
 	public boolean isConnected() {
 		// The connection was successful if the serial.main object is not null
 		return serial.main != null;
 	}
-	
-	public void connect() {
+
+	public void connect(boolean isRetry) {
 		// Attempt to connect
-		int messageType = JOptionPane.QUESTION_MESSAGE;
-		String[] options = {"Yes", "No"};
-		int code = JOptionPane.showOptionDialog(null, 
-				"Are you connected to the Incufridge?", 
-				"Connected?", 0, messageType, 
-				null, options, "Yes");
+		int code = 0;
+		if (!isRetry) {
+			int messageType = JOptionPane.QUESTION_MESSAGE;
+			String[] options = {"Yes", "No"};
+			code = JOptionPane.showOptionDialog(null, 
+					"Are you connected to the Incufridge?", 
+					"Connected?", 0, messageType, 
+					null, options, "Yes");
+		}
 
 		if (code == 0) {
 			serial.tryConnect();
 		}
-		
+
 		// Update the status based on whether or not the connection succeeded
 		if (isConnected()) {
 			connectionStatus.setText("Connected to Incu-Fridge on port " + serial.port);
@@ -67,14 +70,14 @@ public class ConnectionPanel extends JPanel {
 			connectionStatus.setText("Was not able to connect to Incu-Fridge");
 		}
 	}
-	
+
 	private class AL implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (e.getSource() == retryButton) {
 				System.out.println("Retry button pressed");
-				connect();
+				connect(true);
 			}
 		}
 	}
